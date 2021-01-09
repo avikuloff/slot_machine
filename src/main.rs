1dbea26dccfe460ac6ea7561dac0260704a818c1
Andrew <avikuloff@yandex.ru>
2021-01-09 12:24:00 +0300

    Refactoring ui

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ fn main() {
 
     println!("Your balance: {} credits", balance);
     println!("Bet size: {}", bet_size);
-    println!("To get a balance, put the `balance`");
-    println!("To get a bet size, put the `bet`");
-    println!("To increase or decrease the size of the bet, put `bet plus` or `bet minus`.");
+    print_help();
 
     let bet = Bet::new(bet_size, bet_min, bet_max);
     let mut game = Game::new(balance, bet);
@@ -30,41 +28,68 @@ fn main() {
             "BALANCE" => println!("Your balance: {} credits.", game.credits()),
             "BET" => println!("Current bet: {} credits.", game.bet_size()),
             "BET PLUS" => {
-                bet_plus(&mut game);
-                println!("Bet size: {}.", game.bet_size());
+                match bet_plus(&mut game) {
+                    Ok(val) => println!("Bet size: {}.", val),
+                    Err(e) => println!("{}", e)
+                }
             },
             "BET MINUS" => {
-                bet_minus(&mut game);
-                println!("Bet size: {}.", game.bet_size());
+                match bet_minus(&mut game) {
+                    Ok(val) => println!("Bet size: {}.", val),
+                    Err(e) => println!("{}", e)
+                }
             },
             "SPIN" => {
-                if let Ok(_) = game.spin() {
-                    println!("{:?}", game.symbols());
-                    println!("You win {} credits", game.win());
+                match game.spin() {
+                    Ok(_) => {
+                        println!("{:?}", game.symbols());
+                        println!("You win {} credits", game.win());
+                    }
+                    Err(e) => println!("{}", e.to_owned())
                 }
             }
+            "HELP" => print_help(),
             _ => println!("Invalid command!")
         }
     }
 }
 
-fn bet_plus(game: &mut Game) {
-    match game.bet_size() {
-        1 => game.set_bet_size(2),
-        2 => game.set_bet_size(3),
-        3 => game.set_bet_size(5),
-        5 => game.set_bet_size(10),
-        _ => println!("Invalid bet size!")
-    }
+// Increase bet size
+fn bet_plus(game: &mut Game) -> Result<u32, String> {
+    let bet_size = match game.bet_size() {
+        1 => 2,
+        2 => 3,
+        3 => 5,
+        5 => 10,
+        10 => return Err("Max bet size!".to_owned()),
+        _ => return Err("Invalid bet size!".to_owned())
+    };
+
+    game.set_bet_size(bet_size);
+
+    Ok(bet_size)
 }
 
-fn bet_minus(game: &mut Game) {
-    match game.bet_size() {
-        10 => game.set_bet_size(5),
-        5 => game.set_bet_size(3),
-        3 => game.set_bet_size(2),
-        2 => game.set_bet_size(1),
-        _ => println!("Invalid bet size!")
-    }
+// Decrease bet size
+fn bet_minus(game: &mut Game) -> Result<u32, String> {
+    let bet_size = match game.bet_size() {
+        10 => 5,
+        5 => 3,
+        3 => 2,
+        2 => 1,
+        1 => return Err("Min bet size!".to_owned()),
+        _ => return Err("Invalid bet size!".to_owned())
+    };
+
+    game.set_bet_size(bet_size);
+
+    Ok(bet_size)
+}
+
+// Prints help text
+fn print_help() {
+    println!("To get a balance, put the `balance`");
+    println!("To get a bet size, put the `bet`");
+    println!("To increase or decrease the size of the bet, put `bet plus` or `bet minus`.");
 }
 
